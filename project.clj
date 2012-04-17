9a0b8da927178f4912aa5e0dc626a2ed41c99505
Hakan Raberg <hraberg@yahoo.se>
2012-04-17 20:01:31 +0100

    upgrading to Clojure 1.4

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mimir "0.1.0-SNAPSHOT"
   :description "Mímir is an experimental rule engine written in Clojure"
-  :dependencies [[clojure "1.4.0-beta5"]
+  :dependencies [[clojure "1.4.0"]
                  [log4j/log4j "1.2.16"]
                  [org.clojure/tools.logging "0.2.3"]]
   :dev-dependencies [[marginalia "0.7.0"]
