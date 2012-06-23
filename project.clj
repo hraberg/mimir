daf3a328f411cc0c85aadcb7a88d4da18a395a88
Hakan Raberg <hraberg@yahoo.se>
2012-06-23 19:31:10 +0100

    Leiningen 2

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
-(defproject mimir "0.1.0-SNAPSHOT"
+(defproject mimir/mimir "0.1.0-SNAPSHOT"
   :description "Mímir is an experimental rule engine written in Clojure"
-  :dependencies [[clojure "1.4.0"]
+  :repositories {"sonatype snapshots"
+                 "https://oss.sonatype.org/content/repositories/snapshots/"}
+  :dependencies [[org.clojure/clojure "1.5.0-alpha2"]
                  [log4j/log4j "1.2.16"]
                  [org.clojure/tools.logging "0.2.3"]]
-  :dev-dependencies [[marginalia "0.7.0"]
-                     [lein-difftest "1.3.7"]]
+  :profiles {:dev {:dependencies [[marginalia "0.7.1"]]}}
+  :plugins [[lein-difftest "1.3.8"]]
   :repl-init mimir.well
+  :aot [mimir.well]
   :main mimir.well
-  :aot [mimir.well])
+  :min-lein-version "2.0.0")
