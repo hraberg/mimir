9ae5227118bc1affc6c28d46d25c83393053a2f3
Håkan Råberg <hraberg@yahoo.se>
2013-04-13 01:42:32 +0530

    Hint where the interesting stuff is

--- test/mimir/test/parse.clj
+++ test/mimir/test/parse.clj
@@ -4,6 +4,8 @@
   (:use [mimir.parse]))
 
 ;; This is not yet a real test, just experiments and examples in various broken states.
+;; See the bottom of this file for an example of the PEG grammar reparsing itself.
+
 ;; This grammar is from the "Transforming the tree" section of Instaparser:
 ;; https://github.com/Engelberg/instaparse#transforming-the-tree
 
